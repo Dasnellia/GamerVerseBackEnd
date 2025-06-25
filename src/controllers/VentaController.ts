e53e58459aa1f602b14276bc95a18a8501d91e12
A15217 <A15217@lab.aca>
2025-06-25 12:52:09 -0500

    Inicio Sesion

--- src/controllers/VentaController.ts
+++ src/controllers/VentaController.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express"
-import { PrismaClient } from "../generated/prisma"
+import { PrismaClient } from '../generated/prisma'
 
 
 const VentasController = () => {
